b9a192b11ac2df394259ab0205a6086d53a94f97
Timothy Harris <timothy.harris@kikoda.com>
2024-06-08 20:26:16 -0400

    fix: removed unused import

--- src/components/ui/button.cy.tsx
+++ src/components/ui/button.cy.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { Button } from "./button";
 
 describe("<Button />", () => {
   it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
     cy.mount(
       <div className="h-screen bg-slate-800 p-11">
         <Button variant="outline">Test</Button>
