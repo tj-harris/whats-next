bfec42b889a81b139d2dfdaac1a3e595f269cde0
Timothy Harris <timothy.harris@kikoda.com>
2024-06-09 07:08:44 -0400

    fix: updated basepath for gh pages deploy, updated cahce node_modules

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, basepath: "/whats-next" });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  base: "/whats-next",
   resolve: {
     alias: {
       "@": resolve(__dirname, "/src"),
